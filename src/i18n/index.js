6b54db4a41d6006e6e177aa32680d97c2b37aea8
Ezequiel Leites <leites25@gmail.com>
2018-11-28 15:58:14 -0300

    Commit inicial

---
+++ src/i18n/index.js
@@ -0,0 +1,5 @@
+class i18n {
+
+}
+
+export default i18n;
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+// Imports: Dependencies
+const path = require('path');
+require("babel-register");
+// Webpack Configuration
+const config = {
+  mode: 'development',
+  // Entry
+  entry: './src/index.js',
+  // Output
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  // Loaders
+  module: {
+    rules : [
+      // JavaScript/JSX Files
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      // CSS Files
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'sass-loader']
+      }
+    ]
+  },
+
+  // OPTIONAL
+  // Reload On File Change
+  watch: true,
+  // Development Tools (Map Errors To Source File)
+  devtool: 'source-map',
+};
+// Exports
+module.exports = config;
